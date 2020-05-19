8c7383a76e7a780230a6487136b1d009e88a09e5
sam brodie <brodie677@hotmail.com>
2020-05-19 01:27:54 +0100

    problem 1 from https://www.shiftedup.com/2015/05/07/five-programming-problems-every-software-engineer-should-be-able-to-solve-in-less-than-1-hour

--- mycpp.cpp
+++ mycpp.cpp
@@ -2,20 +2,48 @@
 //
 
 #include <iostream>
+#include <vector>
+using namespace::std;
+
+inline void takeInputs(std::vector<int> &v)
+{
+    int num;
+    while (std::cin >> num)
+    {
+        v.push_back(num);
+    }
+}
+
+int recFunc(std::vector<int> v, int pos)
+{
+    if (pos == v.size()) return 0;
+    return v[pos] + recFunc(v, pos + 1);
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
-    while (1);
+    std::vector<int> numbers;
+    std::cout << "Hello World!" << endl;
+    takeInputs(numbers);
+
+    int sum = 0;
+    //for loop
+    //for (int i = 0; i < numbers.size(); i++) {
+    //    sum += numbers[i];
+    //}
+
+    //while loop
+    //int cnt = 0;
+    //while(cnt < numbers.size()) {
+    //    sum += numbers[cnt];
+    //    cnt++;
+    //}
+
+    //recursion
+    //sum = recFunc(numbers, 0);
+
+    std::cout << "sum is : " << sum << endl;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
